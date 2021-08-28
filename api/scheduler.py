3db5d3fc469122cac2a1b839f93646e2f2570df8
aniket <dummyani26@gmail.com>
2021-08-28 19:24:58 +0530

    Fetch new raw data using threads
    This starts up the server faster and also speeds up fetch_new_raw_data
    periodic job.

--- api/scheduler.py
+++ api/scheduler.py
@@ -1,3 +1,5 @@
+import concurrent.futures
+
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from api.lstm_registry import lstm_registry
@@ -7,8 +9,11 @@ from api.models import MAX_NUM_OF_PREDICTIONS, Prediction, Ticker
 # If get_raw_df takes more time to complete than the interval of this func then - Execution of job "fetch_new_raw_data (trigger: interval[0:00:05], next run at: 2021-08-22 12:57:04 IST)" skipped: maximum number of running instances reached (1)
 def fetch_new_raw_data():
     symbols = Ticker.objects.values_list('symbol', flat=True)
-    for symbol in symbols:
-        lstm_registry.get_service_by_symbol(symbol).preprocessed_data.data_processor.raw_data_source.get_raw_df()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(fetch_new_raw_data_from_symbol, symbols)
+
+def fetch_new_raw_data_from_symbol(symbol):
+    lstm_registry.get_service_by_symbol(symbol).preprocessed_data.data_processor.raw_data_source.get_raw_df()
 
 
 def delete_prediction_objects():
