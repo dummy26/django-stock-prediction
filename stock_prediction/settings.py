e39432b8cd919628ad60895b7d04b35fcf0bd3d0
aniket <dummyani26@gmail.com>
2021-10-20 12:05:07 +0530

    Add heroku app to CORS

--- stock_prediction/settings.py
+++ stock_prediction/settings.py
@@ -133,6 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://react-stock-prediction.herokuapp.com/"
 ]
 
 STATICFILES_DIRS = [
