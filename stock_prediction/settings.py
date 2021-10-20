a32495d49195d2e9540ba8d631739b0487293f66
aniket <dummyani26@gmail.com>
2021-10-20 10:13:14 +0530

    Add django_heroku

--- stock_prediction/settings.py
+++ stock_prediction/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,5 @@ CORS_ALLOWED_ORIGINS = [
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'react/build/static')
 ]
+
+django_heroku.settings(locals())
