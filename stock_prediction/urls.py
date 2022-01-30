e622c0cfa0ee0ef1914390eb5ebb59812531bdb6
aniket <dummyani26@gmail.com>
2022-01-30 22:37:45 +0530

    Add homepage

--- stock_prediction/urls.py
+++ stock_prediction/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="home.html"), name='home'),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
