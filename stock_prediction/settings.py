7f4a85cde86de4f1a513bbc5cb9eb47345612ee5
aniket <dummyani26@gmail.com>
2022-02-02 20:11:56 +0530

    Change CORS allowed origins

--- stock_prediction/settings.py
+++ stock_prediction/settings.py
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    'https://ani-stock-prediction.herokuapp.com'
+    'https://react-stock-prediction.herokuapp.com'
 ]
 
 django_heroku.settings(locals())
